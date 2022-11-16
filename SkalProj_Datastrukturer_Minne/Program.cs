ae1c651e6bf42e785220733dad155a28023b03c0
Johan Svensson <hyperjohan@hotmail.com>
2022-11-16 17:24:22 +0100

    Övning 1: ExamineList()

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -4,6 +4,42 @@ namespace SkalProj_Datastrukturer_Minne
 {
     class Program
     {
+
+        /*
+         * 
+         * 
+
+        Frågor:
+
+        1. Hur fungerarstackenochheapen? Förklaragärna med exempel eller skiss på dess grundläggande funktion.
+
+        Svar: 
+
+        Stacken är som en hög med lådor ovanpå varandra. Använder principen "först in sist ut". För att komma åt lådor längre ner måste man flytta på lådorna över dessa. När en låda (anrop, metoder) använts (körts) kastas den.
+
+        Heapen är mer av en mappstruktur/trädstruktur men där man har tillgång till allt innehåll direkt. Som tvättade kläder utlagda i en trädstruktur på en säng, där det är enkelt att ta det man vill ha.
+
+
+        2.Vad är Value Types respektive Reference Types och vad skiljer dem åt?
+
+        Reference Types kan bara existera på Heapen medan Value Types kan skapas på Stacken men också inuti en Reference Type på Heapen.
+
+        Value Types är ifrån System.ValueTypes. 
+
+        Reference Types är typer som ärver ifrån System.Object (.object). Reference Types har en pekare som anger var i minnet (Heapen) det är allokerat. 
+        
+
+        3. Följande metoder (se bild nedan) genererar olikasvar. Den första returnerar 3, denandra returnerar 4, varför?
+
+        Det första exemplet lagras på stacken och det skapas 2 olika minnes platser för de olika variablerna.
+
+        Det andra exemplet lagras i ett objekt på Heapen. Pekaren ändrar på den minnesplats som pekas ut. 
+
+
+
+        */
+
+
         /// <summary>
         /// The main method, vill handle the menues for the program
         /// </summary>
@@ -63,22 +99,61 @@ namespace SkalProj_Datastrukturer_Minne
         static void ExamineList()
         {
             /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch statement with cases '+' and '-'
-             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
-             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
-             * In both cases, look at the count and capacity of the list
-             * As a default case, tell them to use only + or -
-             * Below you can see some inspirational code to begin working.
+             Loop this method until the user inputs something to exit to main menue.
+             Create a switch statement with cases '+' and '-'
+             '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             In both cases, look at the count and capacity of the list
+             As a default case, tell them to use only + or -
+             Below you can see some inspirational code to begin working.
+
+
+            Svar:
+
+            1. Klart
+            2. Ökar när jag matar in 5:e
+            3. 4
+            4. Antar att det är en array med 4 platser som läggt på
+            5. Nej
+            6. Om man vill spara minne
+
+
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            Console.WriteLine("Mata in + eller -. Tillbaka, skriv Q");
+            bool ExitExamineList = false;
+            List<string> theList = new List<string>();
 
-            //switch(nav){...}
-        }
+            do
+            {
+                            
+                string input = Console.ReadLine()!;
+                char nav = input[0];
+                string value = input.Substring(1);
+
+
+                
+
+                switch (nav) {
+                    case '+':
+                            theList.Add(value);
+                        break;
+                    case '-':
+                            theList.Remove(value);
+                        break;
+                    case 'Q':
+                            ExitExamineList = true;
+                        break;
+                    case 'q':
+                            ExitExamineList = true;
+                        break;
+                }
+                Console.WriteLine($"Listan innehåller:");
+
+                theList.ForEach(item => { Console.WriteLine($"{item}"); });
+
+            } while (!ExitExamineList);
+    }
 
         /// <summary>
         /// Examines the datastructure Queue
